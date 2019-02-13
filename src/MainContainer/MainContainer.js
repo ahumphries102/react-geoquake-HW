b7c36153e4865556905eaecdb5ca11a5b4afdc32
AJ <ahumphries102@gmail.com>
2019-02-13 16:38:58 -0600

    First commit!

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
- 
+ state = {
+
+  }
+  getEarthQuakes = async ()=>{
+    try{
+      const earthquakesApi = await fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02')
+      const earthquakeData = await earthquakeApijson()
+    }
+  }
 
   render() {
     return (
